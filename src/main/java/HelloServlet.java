d97fa1a6510d649d582b6ac5e704f4dab2fc1683
Yavor Stanoev <yavor.stan@gmail.com>
2019-12-19 19:03:47 +0200

    New DB diagram + some configuration

---
+++ src/main/java/HelloServlet.java
@@ -0,0 +1,11 @@
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+    public void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse)
+            throws IOException {
+        httpServletResponse.getWriter().print("Hello from servlet");
+    }
+}
